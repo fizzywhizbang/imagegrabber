2cf20306940e20414815e6e0a9a4265b5ada730a
Marc Levine <levinems@3sys.com>
2018-09-30 07:10:01 -0500

    initial upload

---
+++ imagegrabber.py
@@ -0,0 +1,112 @@
+#!/usr/bin/env python3
+import requests
+try:
+    from StringIO import BytesIO
+except ImportError:
+    from io import BytesIO
+
+from bs4 import BeautifulSoup
+from fake_useragent import UserAgent
+import random
+from time import sleep
+import sys
+import re, os
+from datetime import datetime
+from six.moves.urllib.parse import urljoin, urlsplit, SplitResult
+
+
+ua = UserAgent() # From here we generate a random user agent
+proxies = [] # Will contain proxies [ip, port]
+
+class colorize:
+    WARNING="\033[1;0;31m"
+    NORMAL="\033[0;0;0m"
+    GREEN="\033[1;0;32m"
+
+def imagegrabber(url, fname, dir):
+
+    print("image grab url=%s" % url)
+    proxy = getProxy()
+    header = {'User-Agent': str(ua.random)}
+    data = requests.get(url, headers=header, proxies=proxy)
+    if os.path.isfile(fname):
+        fname = str(datetime.now()) + fname
+        fname = fname.replace(":","")
+    if dir == ".":
+        open(fname, 'wb').write(data.content)
+    else:
+        open(dir + "/" + fname, 'wb').write(data.content)
+
+
+
+    return "complete"
+
+
+# Main function
+def getProxy():
+    # Retrieve latest proxies
+    try:
+        url = 'https://www.sslproxies.org/'
+        header = {'User-Agent': str(ua.random)}
+        response = requests.get(url, headers=header)
+        soup = BeautifulSoup(response.text, 'lxml')
+        proxies_table = soup.find(id='proxylisttable')
+
+        # Save proxies in the array
+        for row in proxies_table.tbody.find_all('tr'):
+            proxies.append({
+                'ip':   row.find_all('td')[0].string,
+                'port': row.find_all('td')[1].string
+            })
+
+        # Choose a random proxy
+        proxy = random.choice(proxies)
+        return proxy
+    except:
+        #slow down proxy requests
+        sleep(2)
+        getProxy()
+
+
+def scanurl(url):
+    header = {'User-Agent': str(ua.random)}
+    response = requests.get(url, headers=header)
+    soup = BeautifulSoup(response.text, 'lxml')
+    for link in soup.find_all('a', href=True):
+        if link['href'].endswith(fileList):
+            fname = getName(link['href'])
+            imagegrabber(link['href'], fname, dir)
+
+def getName(url):
+    fields = urlsplit(url)._asdict()  # convert to absolute URLs and split
+    fields['path'] = re.sub(r'/$', '', fields['path'])  # remove trailing /
+    fname = fields['path'].split("/")
+    # figure out which is the last
+    last = len(fname) - 1
+    return fname[last]
+
+def getDir(url):
+    fields = urlsplit(url)._asdict()  # convert to absolute URLs and split
+    fields['path'] = re.sub(r'/$', '', fields['path'])  # remove trailing /
+    fname = fields['path'].split("/")
+    # figure out which is the last
+    last = len(fname) - 1
+    return fname[last]
+
+url = sys.argv[1]
+
+if len(sys.argv) == 3:
+    dir = sys.argv[2]
+    if not os.path.exists(dir):
+        os.mkdir(dir)
+else:
+    dir = getDir(url)
+    if not os.path.exists(dir):
+        os.mkdir(dir)
+
+fileList = ('jpg','JPG','jpeg','JPEG','mp4','m4v','mov','wmv')
+
+
+scanurl(url)
+#returnval = imagegrabber(url, fname[last], dir)
+#print(returnval)
